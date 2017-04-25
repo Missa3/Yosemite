007778e78679f8c107661827565dc26e3e68f0bd
Missael Romero <missael7994@gmail.com>
2017-04-25 13:21:29 -0700

    hover added

--- landing.js
+++ landing.js
@@ -1,13 +1,47 @@
 var pics = [
     'ypics/hiking.png',
-    'ypics/animals.png',
-    'ypics/yosemitefalls.png'
+    'ypics/yosemitefalls.png',
+    'ypics/animals.png'
 ]
 
 var box = document.getElementsByClassName('box');
-console.log(box);
+var main = document.getElementById('content');
+var menu = document.getElementById('menu');
+var nav = document.getElementById('nav');
 
 for (var i = 0; i < box.length; i++) {
     box[i].style.backgroundImage = "url(" + pics[i] + ")";
-
+    hover(box[i]);
 };
+
+function hover(x) {
+    var hr = x.childNodes[1].childNodes[3];
+    x.addEventListener("mouseenter", function() {
+        this.childNodes[1].className += " dark";
+        hr.style.width = "30%";
+    });
+    x.addEventListener("mouseleave", function() {
+        this.childNodes[1].classList.remove("dark");
+        hr.style.width = "50%";
+    });
+}
+hover(main);
+
+menu.addEventListener("click", function() {
+    // var nav = this.previousElementSibling;
+    if (nav.style.display == "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+});
+// 
+// for (var i = 0; i < nav.children.length; i++) {
+//     nav.children[i].addEventListener("mouseenter", function() {
+// this.innerHTML="<hr>" + this.textContent + "<hr>";
+//     });
+//     nav.children[i].addEventListener("mouseleave", function() {
+//
+//
+//     });
+// }
